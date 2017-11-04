2e529a61a36dae923efff73d32baf095906c05b6
Denis Nikolskiy <nikolskiy.development@gmail.com>
2017-11-04 16:39:40 +0300

    init commit

---
+++ app/src/main/java/ru/denisnikolskiy/flexiblealarm/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.denisnikolskiy.flexiblealarm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
